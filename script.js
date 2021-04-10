001f984febc7c6706b4f9110cbd32e55870d3fe2
joel <joel7797@gmail.com>
2021-04-10 20:06:04 +0530

    First Commit : First Version

---
+++ script.js
@@ -0,0 +1,18 @@
+function validate(redirect)
+{
+    var user= document.getElementById("user");
+    var password= document.getElementById("pass");
+    if( user.value=="admin" && password.value =="12345")
+    {
+        redirect();
+    }
+    else
+    {
+        alert("Enter a valid username & password");
+    }
+}
+
+ function redirect() 
+ {
+    location.replace ("main.html");
+ }
